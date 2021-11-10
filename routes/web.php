281227f7fbb7460631731f8985676e0901518531
kanekonanami <n-kaneko@jutjoy.com>
2021-11-10 18:24:10 +0900

    4-10

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit' , 'ProfileController@edit');
+  });
